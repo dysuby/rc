563a63c7a117362aadbef1e6f5b26812dfaf7793
vegchic <h_foryou@163.com>
2022-03-20 18:04:38 +0800

    update

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,14 @@
 Import-Module PSReadLine
+
 Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+
+
+$env:HTTP_PROXY = "http://127.0.0.1:8888"
+$env:HTTPS_PROXY = "http://127.0.0.1:8888"
 
 if ($env:__INTELLIJ_COMMAND_HISTFILE__ -or $env:TERM_PROGRAM -eq "vscode") {
     Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/material.omp.json")
-} else {
+}
+else {
     Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/sukyun.omp.json")
 }
