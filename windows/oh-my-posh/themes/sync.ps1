15c1c4b7723aa0e0189d62cc8b325a1c31e30847
vegchic <h_foryou@163.com>
2023-05-28 21:15:37 +0800

    update

--- windows/oh-my-posh/themes/sync.ps1
+++
@@ -1,9 +0,0 @@
-# oh-my-posh themes
-Get-Content $PROFILE | Select-String -Pattern '\$\(scoop prefix oh\-my\-posh\)\/themes\/[\w.]+\.json' -AllMatches | 
-ForEach-Object { $_.Matches } | 
-ForEach-Object { $_.Value } | 
-ForEach-Object {
-    Write-Host "copying $_"
-    $realPath = $ExecutionContext.InvokeCommand.ExpandString($_)
-    Copy-Item $realPath $PSScriptRoot
-}
--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -6,7 +6,7 @@ Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 $env:HTTP_PROXY = "http://127.0.0.1:8888"
 $env:HTTPS_PROXY = "http://127.0.0.1:8888"
 
-Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/material.omp.json")
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\material.omp.json" | Invoke-Expression
 
 # pipenv default venv dir
 $Env:WORKON_HOME = "D:\.virtualenvs"
