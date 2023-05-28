e1b2e592c6ecc16fd5f0614b016da296542ca6b6
vegchic <h_foryou@163.com>
2023-05-28 14:22:00 +0800

    update

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -6,9 +6,12 @@ Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
 $env:HTTP_PROXY = "http://127.0.0.1:8888"
 $env:HTTPS_PROXY = "http://127.0.0.1:8888"
 
-if ($env:__INTELLIJ_COMMAND_HISTFILE__ -or $env:TERM_PROGRAM -eq "vscode") {
-    Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/material.omp.json")
-}
-else {
-    Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/sukyun.omp.json")
-}
+Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/material.omp.json")
+
+# pipenv default venv dir
+$Env:WORKON_HOME = "D:\.virtualenvs"
+$Env:PIP_CACHE_DIR = "D:\cache"
+$Env:PIPENV_CACHE_DIR = "D:\cache"
+
+# diable update check
+$Env:POWERSHELL_UPDATECHECK = "Off"
